0668400aaef887fbf691ac773b95c9ce0b8f9294
star-7prog <rahmadpanjisurya1@gmail.com>
2024-10-31 13:17:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+let randomNumber =
+Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
+const maxAttempst = 10;
+
+function checkGuess() {
+    let userGuess=
+    document.getElementById('guess').value;
+    let result =
+    document.getElementById('result');
+    let attemptsDisplay =
+    document.getElementById('attempts');
+        attempts++;
+    
+    if (userGuess== randomNumber) {
+        result.textContent = 'Selamat Anda menebak dengan benar';
+        result.style.color = 'green';
+        endGame();
+
+    } else if (userGuess > randomNumber ) {
+        result.textContent = 'Terlalu tinggi! Coba lagi.';
+        result.style.color = 'red';
+    } else {
+        result.textContent = 'Terlalu rendah! Coba lagi.';
+        result.style.color = 'red';    
+    }
+
+    attemptsDisplay.textContent = `Percobaan: ${attempts} dari ${maxAttempts}`;
+    if (attempts >= maxAttempts) {
+        result.textContent = `Anda kalah! Angka yang benar adalah ${randomNumber}.`;
+        result.style.color = 'cyan';
+        endGame();       
+     }
+}
+
+function endGame() {
+    document.getElementById('guess').disabled = true;
+
+document.getElementById('restart').classList.remove('hidden');
+}
+
+function restartGame() {
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+    attempts = 0;
+    document.getElementById('guess').disabled = false;
+    document.getElementById('result').textContent = '';
+    document.getElementById('attempts').textContent = '';
+
+document.getElementById('restart').classList.add('hidden');
+}
